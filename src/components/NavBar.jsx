01fa55e10b56e0beb75bda114e7e5b7a8d62711f
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2022-08-29 13:53:28 +0000

    Navbar created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import {Stack} from '@mui/material'
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import logo from '../assets/logo.png'
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
-    <div style={{color: 'red'}}>Navbar</div>
-  )
-}
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        top: 0,
+        justifyContent: "space-between",
+        background: "#C8A2C8",
+      }}>
+      <Link to={"/"} style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45}/>
+      </Link>
+      <SearchBar/>
+    </Stack>
+  );
+};
 
-export default NavBar
+export default NavBar;
